e68ddcf5d9ab3d4f0b4815832ca6c0f801a5bdf8
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-10-15 08:21:22 +0530

    l 15

--- L-15_Js/script.js
+++ L-15_Js/script.js
@@ -1,5 +1,23 @@
-let a="ankit singh"
-for (let i of a){
-    console.log(i);
+try {
+    var x = 20;
+    console.log(x);
+    function fun(){
+        var a = 10;
+        console.log(a);
+        console.log(x);
+    }
+    fun();
+    console.log(p);
+    var p =30;
+    console.log(p);
+    console.log(a);
+    console.log('end');
+} catch (error) {
+    // console.log(error);
 }
+finally{
+    console.log("alksdhjdsj");
+}
+
+
 
