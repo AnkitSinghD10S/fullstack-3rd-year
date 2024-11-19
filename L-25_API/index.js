cfa6726c0d32cb8a69654e43bc21ca44471eae4b
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-11-19 09:39:16 +0530

    API fetch

--- L-25_API/index.js
+++ L-25_API/index.js
@@ -1,16 +1,37 @@
-const btn = document.querySelector('.btn')
-const list = document.querySelector('.list');
-
+const btn = document.querySelector(".btn");
+const list = document.querySelector(".list");
+const input = document.querySelector(".input");
 async function fun() {
     try {
-        const data =await fetch("https://jsonplacder.typicode.com/users")
-        const l=await data.json();
-        for(let i=0;i<l.length;i++){
-            const li = document.createElement('li');
-            li.innerText=l[i].name;
-            list.appendChild(li) 
+        const data = await fetch("http://universities.hipolabs.com/search?");
+        const l = await data.json();
+        let input_name =input.value
+        const div =document.createElement('div');
+        const name = document.createElement('div');
+        const state = document.createElement('div');
+        const webpage = document.createElement('div');
+        for (let i = 0; i < l.length; i++) {
+            console.log(l[i]);
+            if(l[i].country==(input_name)){
+                const C_name = document.createElement("li");
+                C_name.innerText = l[i].name;
+                list.appendChild(li);
+                const C_state= document.createElement("li");
+                C_state.innerText = l[i].country;
+                list.appendChild(li);
+                const C_webpage= document.createElement("li");
+                C_webpage.innerText = l[i]['web_pages'][0];
+                name.appendChild(C_name);
+                state.appendChild(C_state);
+                webpage.appendChild(C_webpage);
+            }
+            div.style.display='flex';
+            div.appendChild(name);
+            div.appendChild(state);
+            div.appendChild(webpage);
+            list.appendChild(div)
         }
     } catch (error) {
-        console.log(error," error has occured ");
+        console.log(error, " error has occured ");
     }
 }
