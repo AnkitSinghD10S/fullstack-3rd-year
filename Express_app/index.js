58e1d8bf91f284b008bde3cfb67bf3f94a32913c
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-01-10 13:45:31 +0530

    refactor routing setup and add basic GET endpoint

--- Express_app/index.js
+++ Express_app/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import route from "./route.js";
 const app = express();
 
-app.use("/", route.Router);
+app.use("/", route);
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
